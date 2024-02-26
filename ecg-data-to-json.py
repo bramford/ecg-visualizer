508934caacda57ce7a4ceca92a9e783be9371d2c
Bramford Horton <bram.horton@derp.tech>
2024-02-27 00:40:59 +1300

    Convert ~100 samples to JSON

--- ecg-data-to-json.py
+++ ecg-data-to-json.py
@@ -2,6 +2,7 @@
 import scipy.io
 import json
 import re
+import os
 #mat = scipy.io.loadmat('./MLII/1 NSR/100m (0).mat')
 
 import numpy as np
@@ -17,48 +18,59 @@ class NpEncoder(json.JSONEncoder):
         return super(NpEncoder, self).default(obj)
 
 
-mat = scipy.io.loadmat('./physionet.org/files/ecg-arrhythmia/1.0.0/WFDBRecords/01/010/JS00001.mat')
-readings = []
+def convert_sample_to_json(samplePath: str):
+    sampleId = os.path.splitext(os.path.basename(samplePath))[0]
+    parentDir = os.path.dirname(samplePath)
+    mat = scipy.io.loadmat(samplePath)
+    readings = []
 
-for value in mat["val"]:
-    for v in value:
-        readings.append(v)
+    for value in mat["val"]:
+        for v in value:
+            readings.append(v)
 
-jsondata = dict()
-jsondata["readings"] = readings
-jsondata["leads"] = []
+    jsondata = dict()
+    jsondata["readings"] = readings
+    jsondata["leads"] = []
 
-with open("./physionet.org/files/ecg-arrhythmia/1.0.0/WFDBRecords/01/010/JS00001.hea", "r", encoding="UTF-8") as file:
-    lines = [line.rstrip() for line in file]
-    for i, line in enumerate(lines):
-        if i == 0:
-            machinematch = re.match(r'^(JS\d+)\s+(\d+)\s+(\d+)\s+(.*)', line)
-            if machinematch:
-                jsondata["leadNumber"] = machinematch.group(2)
-                jsondata["sampleRate"] = machinematch.group(3)
-                jsondata["otherMachineData"] = machinematch.group(4)
+    with open(os.path.join(parentDir,sampleId) + ".hea", "r", encoding="UTF-8") as file:
+        lines = [line.rstrip() for line in file]
+        for i, line in enumerate(lines):
+            if i == 0:
+                machinematch = re.match(r'^(JS\d+)\s+(\d+)\s+(\d+)\s+(.*)', line)
+                if machinematch:
+                    jsondata["leadNumber"] = machinematch.group(2)
+                    jsondata["sampleRate"] = machinematch.group(3)
+                    jsondata["otherMachineData"] = machinematch.group(4)
+                    continue
+
+            if i > 0 and i < 13:
+                leadmatch = re.match(r'^(JS\d+\.mat)\s+(.*)', line)
+                if leadmatch:
+                    jsondata["leads"].append(leadmatch.group(2))
+                    continue
+
+            agematch = re.match(r'^\#Age\:\s+(\d+)', line)
+            if agematch:
+                jsondata["age"] = agematch.group(1)
                 continue
 
-        if i > 0 and i < 13:
-            leadmatch = re.match(r'^(JS\d+\.mat)\s+(.*)', line)
-            if leadmatch:
-                jsondata["leads"].append(leadmatch.group(2))
+            sexmatch = re.match(r'^\#Sex\:\s+(\w+)', line)
+            if sexmatch:
+                jsondata["sex"] = sexmatch.group(1)
                 continue
 
-        agematch = re.match(r'^\#Age\:\s+(\d+)', line)
-        if agematch:
-            jsondata["age"] = agematch.group(1)
-            continue
+            dxmatch = re.match(r'^\#Dx\:\s+(\d+.*)', line)
+            if dxmatch:
+                jsondata["diagnoses"] = dxmatch.group(1).split(",")
+                continue
 
-        sexmatch = re.match(r'^\#Sex\:\s+(\d+)', line)
-        if sexmatch:
-            jsondata["sex"] = sexmatch.group(1)
-            continue
+    with open("./" + sampleId + ".json", "w") as f:
+        json.dump(jsondata, f, cls=NpEncoder)
 
-        dxmatch = re.match(r'^\#Dx\:\s+(\d+.*)', line)
-        if dxmatch:
-            jsondata["diagnoses"] = dxmatch.group(1).split(",")
-            continue
-
-with open("./JS00001.json", "w") as f:
-    json.dump(jsondata, f, cls=NpEncoder)
+for root, dirs, files in os.walk("./physionet.org/files/ecg-arrhythmia/1.0.0/WFDBRecords/01/010/"):
+    for file in files:
+        extension = os.path.splitext(file)[1]
+        if extension and extension == ".mat":
+            filePath = os.path.join(root, file)
+            print("Found mat file to convert: " + filePath)
+            convert_sample_to_json(os.path.join(root, file))
--- next-fullstack/src/app/page.tsx
+++ next-fullstack/src/app/page.tsx
@@ -4,7 +4,6 @@ import * as EcgChart from "./components/ecgChart";
 function Footer() {
   return (
       <div className="flex flex-col w-full justify-start bg-gray-800 lg:h-16">
-        <hr className="h-px mb-2 bg-gray-400 border-0"/>
         <div className="flex flex-row justify-center">
           {/*
           <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -35,21 +34,45 @@ function Footer() {
       )
 }
 
-export default async function Home() {
-  const ecgs = await (await fetch(`http://localhost:3002/ecgs`, { cache: 'no-store' })).json()
+function Header() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-gray-900">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="flex flex-col w-full justify-start bg-gray-300 dark:bg-gray-800 lg:h-16">
+        <div className="ml-4 flex flex-row justify-between">
         {/*
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
         */}
-        <p className="text-xl">
-          Homes
+        <a
+          className="pointer-events-none ml-4"
+          href="https://derp.tech"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/heart-anatomy.png"
+            alt="Heart"
+            className="hover:border-sky-50 hover:opacity-95"
+            width={40}
+            height={40}
+            priority
+          />
+        </a>
+        <p className="text-xl dark:invert">
+          ECG Visualizer
         </p>
+        </div>
+        <hr className="h-px mt-2 bg-gray-400 border-0"/>
       </div>
+      )
+}
+
+export default async function Home() {
+  const ecgs = await (await fetch(`http://localhost:3002/ecgs`, { cache: 'no-store' })).json()
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-gray-200 dark:bg-gray-900">
+      <Header/>
 
       <div className="flex justify-center">
         <EcgChart.default readings={ecgs[0].readings}/>
